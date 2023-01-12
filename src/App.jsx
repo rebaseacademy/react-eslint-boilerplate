0c151af59f4fb8111f3c809d15b6f555941a4cdd
Gaston Che <gastonnkh@gmail.com>
2023-01-12 20:53:15 +0100

    basic moviedb done

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from '@emotion/styled';
+import { useEffect, useState } from 'react';
+import { getPopularMovies, searchMovies } from './api';
+import Header from './components/Header';
+import Movie from './components/Movie';
+import SearchForm from './components/SearchForm';
+
+const MoviesWrapper = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 40px 10px;
+  justify-content: center;
+  margin: 50px 0;
+`;
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState('');
+  useEffect(() => {
+    getPopularMovies()
+      .then((data) => {
+        setMovies(data);
+      })
+      .catch(() => {
+        setError('There was an error getting results');
+      });
+  }, []);
+
+  const findMovies = (term) => {
+    searchMovies(term)
+      .then(setMovies)
+      .catch(() => {
+        setError('Error searching movies');
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header>
+        <SearchForm onSearch={findMovies} />
+      </Header>
+      <MoviesWrapper>
+        {error && <div>{error}</div>}
+        {!error &&
+          movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              title={movie.title}
+              description={movie.overview}
+              image={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+            />
+          ))}
+      </MoviesWrapper>
+    </>
   );
 }
 
